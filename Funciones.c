5a09a726a98c29b60420efbfcf794b9e665aaf10
logralahad <61020509+logralahad@users.noreply.github.com>
2020-04-23 10:12:05 -0500

    Update Funciones.c

--- Funciones.c
+++ Funciones.c
@@ -1,5 +1,17 @@
-
+#include <math.h>
 #define PI 3.1416
+
+float volumen_cono(float radio_base_cono, float altura_cono){
+    float vol_cono;
+    vol_cono = (altura_cono * M_PI * pow(radio_base_cono, 2)) / 3;
+    return vol_cono;
+}
+
+float volumen_prisma(float area_base_prisma, float altura_prisma){
+    float vol_prisma;
+    vol_prisma = area_base_prisma * altura_prisma;
+    return vol_prisma;
+}
 
 float volumenDeEsfera(float radio) {
   return (4 * (PI * radio*radio*radio)) / 3;
